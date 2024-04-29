1203ef4ebebb89c58e4f28108fb6f2c5f0b4a324
clomollo <oyoocliff471@gmail.com>
2024-04-29 14:50:16 +0300

    Commit

--- fib.go
+++ fib.go
@@ -1,10 +1,15 @@
 package piscine
 
-
-func Fibonacci(num int)int{
-	if num < 0{
+func Fibonacci(num int) int {
+	if num < 0 {
 		return -1
 	}
+	if num == 1 {
+		return 1
+	}
+	if num == 0 {
+		return 0
+	}
 
-	return Fibonacci(num-1)+Fibonacci(num-2)
+	return Fibonacci(num-1) + Fibonacci(num-2)
 }
